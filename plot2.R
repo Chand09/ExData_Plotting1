b82ddc8b3f015df30312f1d13877e99a903ceb21
Chand09 <40521926+Chand09@users.noreply.github.com>
2018-07-17 01:22:52 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -3,7 +3,6 @@ dat1$Date<-as.Date(dat1$Date,format="%d/%m/%Y")
 dat1$Time<-strptime(paste(dat$Date,dat$Time), format="%d/%m/%Y %H:%M:%S")
 d<-subset(dat1,Date>='2007-02-01')
 d<-subset(d,Date<='2007-02-02')
-`
 plot(d$Time,as.numeric(as.character(d$Global_active_power)),type="l",ylab = "Global Active Power (kilowatts)", xlab="")
 dev.copy(png,file="plot2.png",height=480,width=480,unit="px")
 dev.off()
